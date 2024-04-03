52f7794bfdbb51b7ea6ecca52f8e9b498ec0318f
Kauã Araujo Casimiro <kaua.casimiro16@gmail.com>
2024-04-02 23:40:56 -0300

    nomes modificados

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,13 +31,13 @@ namespace JoKenPo
         {
             label1 = new System.Windows.Forms.Label();
             label2 = new System.Windows.Forms.Label();
-            radioButton1 = new System.Windows.Forms.RadioButton();
-            radioButton2 = new System.Windows.Forms.RadioButton();
-            radioButton3 = new System.Windows.Forms.RadioButton();
-            button1 = new System.Windows.Forms.Button();
-            label3 = new System.Windows.Forms.Label();
-            label4 = new System.Windows.Forms.Label();
-            label5 = new System.Windows.Forms.Label();
+            pedra = new System.Windows.Forms.RadioButton();
+            papel = new System.Windows.Forms.RadioButton();
+            tesoura = new System.Windows.Forms.RadioButton();
+            enviar = new System.Windows.Forms.Button();
+            player = new System.Windows.Forms.Label();
+            IA = new System.Windows.Forms.Label();
+            resul = new System.Windows.Forms.Label();
             SuspendLayout();
             // 
             // label1
@@ -61,95 +61,95 @@ namespace JoKenPo
             label2.TabIndex = 1;
             label2.Text = "Faça Sua Escolha";
             // 
-            // radioButton1
+            // pedra
             // 
-            radioButton1.AutoSize = true;
-            radioButton1.Location = new System.Drawing.Point(74, 143);
-            radioButton1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            radioButton1.Name = "radioButton1";
-            radioButton1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            radioButton1.Size = new System.Drawing.Size(86, 27);
-            radioButton1.TabIndex = 2;
-            radioButton1.TabStop = true;
-            radioButton1.Text = "Pedra";
-            radioButton1.UseVisualStyleBackColor = true;
+            pedra.AutoSize = true;
+            pedra.Location = new System.Drawing.Point(74, 143);
+            pedra.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            pedra.Name = "pedra";
+            pedra.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            pedra.Size = new System.Drawing.Size(86, 27);
+            pedra.TabIndex = 2;
+            pedra.TabStop = true;
+            pedra.Text = "Pedra";
+            pedra.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // papel
             // 
-            radioButton2.AutoSize = true;
-            radioButton2.Location = new System.Drawing.Point(74, 202);
-            radioButton2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            radioButton2.Name = "radioButton2";
-            radioButton2.Size = new System.Drawing.Size(86, 27);
-            radioButton2.TabIndex = 3;
-            radioButton2.TabStop = true;
-            radioButton2.Text = "Papel";
-            radioButton2.UseVisualStyleBackColor = true;
+            papel.AutoSize = true;
+            papel.Location = new System.Drawing.Point(74, 202);
+            papel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            papel.Name = "papel";
+            papel.Size = new System.Drawing.Size(86, 27);
+            papel.TabIndex = 3;
+            papel.TabStop = true;
+            papel.Text = "Papel";
+            papel.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // tesoura
             // 
-            radioButton3.AutoSize = true;
-            radioButton3.Location = new System.Drawing.Point(74, 257);
-            radioButton3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            radioButton3.Name = "radioButton3";
-            radioButton3.Size = new System.Drawing.Size(108, 27);
-            radioButton3.TabIndex = 4;
-            radioButton3.TabStop = true;
-            radioButton3.Text = "Tesoura";
-            radioButton3.UseVisualStyleBackColor = true;
+            tesoura.AutoSize = true;
+            tesoura.Location = new System.Drawing.Point(74, 257);
+            tesoura.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            tesoura.Name = "tesoura";
+            tesoura.Size = new System.Drawing.Size(108, 27);
+            tesoura.TabIndex = 4;
+            tesoura.TabStop = true;
+            tesoura.Text = "Tesoura";
+            tesoura.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // enviar
             // 
-            button1.Location = new System.Drawing.Point(108, 341);
-            button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            button1.Name = "button1";
-            button1.Size = new System.Drawing.Size(129, 33);
-            button1.TabIndex = 5;
-            button1.Text = "Enviar";
-            button1.UseVisualStyleBackColor = true;
+            enviar.Location = new System.Drawing.Point(108, 341);
+            enviar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            enviar.Name = "enviar";
+            enviar.Size = new System.Drawing.Size(129, 33);
+            enviar.TabIndex = 5;
+            enviar.Text = "Enviar";
+            enviar.UseVisualStyleBackColor = true;
             // 
-            // label3
+            // player
             // 
-            label3.AutoSize = true;
-            label3.Location = new System.Drawing.Point(27, 422);
-            label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            label3.Name = "label3";
-            label3.Size = new System.Drawing.Size(98, 23);
-            label3.TabIndex = 6;
-            label3.Text = "Jogador:";
+            player.AutoSize = true;
+            player.Location = new System.Drawing.Point(27, 422);
+            player.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            player.Name = "player";
+            player.Size = new System.Drawing.Size(98, 23);
+            player.TabIndex = 6;
+            player.Text = "Jogador:";
             // 
-            // label4
+            // IA
             // 
-            label4.AutoSize = true;
-            label4.Location = new System.Drawing.Point(45, 463);
-            label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            label4.Name = "label4";
-            label4.Size = new System.Drawing.Size(43, 23);
-            label4.TabIndex = 7;
-            label4.Text = "IA:";
+            IA.AutoSize = true;
+            IA.Location = new System.Drawing.Point(45, 463);
+            IA.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            IA.Name = "IA";
+            IA.Size = new System.Drawing.Size(43, 23);
+            IA.TabIndex = 7;
+            IA.Text = "IA:";
             // 
-            // label5
+            // resul
             // 
-            label5.AutoSize = true;
-            label5.Location = new System.Drawing.Point(212, 448);
-            label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            label5.Name = "label5";
-            label5.Size = new System.Drawing.Size(109, 23);
-            label5.TabIndex = 8;
-            label5.Text = "Resultado";
+            resul.AutoSize = true;
+            resul.Location = new System.Drawing.Point(212, 448);
+            resul.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            resul.Name = "resul";
+            resul.Size = new System.Drawing.Size(109, 23);
+            resul.TabIndex = 8;
+            resul.Text = "Resultado";
             // 
             // Form1
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             ClientSize = new System.Drawing.Size(389, 535);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(button1);
-            Controls.Add(radioButton3);
-            Controls.Add(radioButton2);
-            Controls.Add(radioButton1);
+            Controls.Add(resul);
+            Controls.Add(IA);
+            Controls.Add(player);
+            Controls.Add(enviar);
+            Controls.Add(tesoura);
+            Controls.Add(papel);
+            Controls.Add(pedra);
             Controls.Add(label2);
             Controls.Add(label1);
             Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
@@ -164,13 +164,13 @@ namespace JoKenPo
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton pedra;
+        private System.Windows.Forms.RadioButton papel;
+        private System.Windows.Forms.RadioButton tesoura;
+        private System.Windows.Forms.Button enviar;
+        private System.Windows.Forms.Label player;
+        private System.Windows.Forms.Label IA;
+        private System.Windows.Forms.Label resul;
     }
 }
 
